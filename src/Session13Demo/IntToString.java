e56dffd7f04e61872163bd234f95f2b7b34dd68a
MithunBurman7 <mithunburman7@gmail.com>
2017-06-27 16:21:03 +0530

    Session13.1

---
+++ src/Session13Demo/IntToString.java
@@ -0,0 +1,22 @@
+package Session13Demo;
+
+public class IntToString {
+
+	public static void main(String[] args) {
+
+int x = 45;   // 45 is integer value
+		
+		
+		String a = String.valueOf(x); 
+			
+		  
+		System.out.println("Int value :"+ x); 
+		
+		System.out.println("Int value convert into String :"+ x); //Display ;
+ 
+		                                                      
+
+	}
+
+}
+
